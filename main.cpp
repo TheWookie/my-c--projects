c36b2575c60f83b548159f834c0aa20df740c6fc
Paul Baker <paul.baker@overstock.com>
2016-03-02 19:54:33 -0700

    Create then destroy window.

--- main.cpp
+++ main.cpp
@@ -11,8 +11,7 @@ int main(int argc, char *args[]) {
   SDL_Window *window = NULL;
   SDL_Surface *screenSurface = NULL;
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    const char *error = SDL_GetError();
-    printf("SDL_ERROR: %s%n", error);
+    printf("SDL couldn't be initialized. SDL_ERROR: %s%n", SDL_GetError());
     return 1;
   }
   window = SDL_CreateWindow(
@@ -20,6 +19,15 @@ int main(int argc, char *args[]) {
       SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
       SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN
   );
-
+  if (window == NULL) {
+    printf("Window couldn't be created. SDL_ERROR: %s%n", SDL_GetError());
+    return 1;
+  }
+  screenSurface = SDL_GetWindowSurface(window);
+  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+  SDL_UpdateWindowSurface(window);
+  SDL_Delay(2000);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   return 0;
 }
