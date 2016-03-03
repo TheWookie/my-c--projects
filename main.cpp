55fdf8047a015fa2f8678a8c85232341e73b8eed
Paul Baker <paul.baker@overstock.com>
2016-03-02 20:11:05 -0700

    Added simple PressEnterToQuit class.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <SDL2/SDL.h>
-#include <stdio.h>
+#include "EnterToQuit.h"
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -8,6 +8,8 @@ const int SCREEN_HEIGHT = 480;
 using namespace std;
 
 int main(int argc, char *args[]) {
+  EnterToQuit holdsOnQuit;
+
   SDL_Window *window = NULL;
   SDL_Surface *screenSurface = NULL;
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
